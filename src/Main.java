a479f00314fcdcb3813a9ac70afab64d080eda5f
jibareekumma <shadowcelebekumma@gmail.com>
2026-09-14 05:59:42 +0100

    built java swing

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Hospital Management System");
+
+        frame.setSize(1000, 700);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+}
